a580c17874693b9b901a823ed9869b1465fba139
hasan.mehedi <hasan.mehedi@bjitgroup.com>
2021-10-20 17:32:48 +0600

    overlay animation is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+// const hamburgerBtn = document.getElementById("hamburger-menu");
+// const overlay = document.getElementsByClassName("overlay");
+//
+// hamburgerBtn.addEventListener("click", () => {
+//   if (hamburgerBtn.classList.contains("open")) {
+//     hamburgerBtn.classList.remove("open");
+//     overlay.classList.add();
+//   } else {
+//     hamburgerBtn.classList.add("open");
+//   }
+// });
+
+$(document).ready(function () {
+  const hamburgerBtn = $("#hamburger-menu");
+  const overlay = $(".overlay");
+
+  hamburgerBtn.click(function () {
+    if (hamburgerBtn.hasClass("open")) {
+      // menu close
+      hamburgerBtn.removeClass("open");
+      overlay.removeClass("fade-in").addClass("fade-out");
+    } else {
+      // menu open
+      hamburgerBtn.addClass("open");
+      overlay.removeClass("fade-out").addClass("fade-in");
+    }
+  });
+});
